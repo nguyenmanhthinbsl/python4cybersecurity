4c7c01474a7f53696a5eb542cb4628c66e8f03b2
thin nguyen <Thinnguyen332000@gmail.com>
2023-07-07 21:15:42 +0700

    update API

--- api/connectAPI.py
+++ api/connectAPI.py
@@ -10,3 +10,30 @@ result = request.json()['drinks']
 instructions = result[0]['strInstructions']
 
 print(f'Instructions: {instructions}')
+
+# craft the request
+
+url = "https://reddit.com/"
+
+c = {
+    'test_cookies':'say something random'
+}
+h = {
+    'User-agent':'Googlebot' #Mozilla/5.0
+}
+response = requests.get(
+    url,
+    cookies=c,
+    headers=h
+)
+
+code = response.status_code
+
+print(code)
+
+if code==200:
+    print("Successfully!")
+    
+#maniputation header
+
+print(response.text)
